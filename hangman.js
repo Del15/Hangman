dd94b9dbed78634611fd2b4200f96c9cbc839526
Del15 <ellisderrick205@gmail.com>
2025-10-15 10:42:15 -0400

    code to choose random word from list

---
+++ hangman.js
@@ -0,0 +1,4 @@
+var Possbible_Words = ["javascript", "glass", "amazing", "pancake"];
+
+var randomIndex = partseInt(Math.random() * Possbible_Words.length);
+var word = Possbible_Words[randomIndex];
